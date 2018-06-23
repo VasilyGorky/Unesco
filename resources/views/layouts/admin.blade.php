e36e2b2d85d2a9482cb713695fc5016cc2f03d6b
VasilyGorky <basilik52@gmail.com>
2018-06-23 17:20:49 +0700

    bug fixed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>UnescoAdmin</title>
 
     <!-- Scripts -->
     <script type="text/javascript" src="{{asset('js')}}/app.js" ></script>
